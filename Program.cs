857c2b38521a4d8f0e5b50b2041a35fb86a31fc5
Sviatlana Strachuk <krautsovasviatlana@gmail.com>
2023-12-22 11:04:23 +0100

    change code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+public class DemantGetStarted
+{
+    private static void Main(string[] args)
+    {
+        IWebDriver driver = new FirefoxDriver();
+        driver.Navigate().GoToUrl("https://www.demant.com/");
+
+        var title = driver.Title; // information about browser
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500); //Waiting Stategies based on page load
+
+        //var acceptBox = driver.FindElement(By.Id("[id=coiPage-3]"));
+        //acceptBox.Click();
+
+        var aboutButton = driver.FindElement(By.Name("About"));
+        aboutButton.Click();
+
+        var managAndGovern = driver.FindElement(By.Name("Management and governance"));
+        managAndGovern.Click();
+
+        var text = driver.FindElement(By.ClassName(".like-h1-xl-light"));
+        Assert.AreEqual("Executive Board and Board of Directors", text);
+
+        var searchElement = driver.FindElement(By.ClassName("search"));
+        searchElement.SendKeys("Remuneration report for 2021");
+        searchElement.Click();
+
+        var searchResult = driver.FindElement(By.ClassName("search-result"));
+        Assert.AreEqual("Remuneration report 'for' 2021");
+
+    }
+}
